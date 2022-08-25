a7bbb621a57ab6179bf8e03f7597efad91983a8b
Gilbert <gssenyonjo@gmail.com>
2022-08-25 07:07:52 +0300

    initial commit. Created index page

---
+++ src/test/java/net/passioncloud/recipe/RecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.passioncloud.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
